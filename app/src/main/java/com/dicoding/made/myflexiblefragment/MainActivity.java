49ed77a08e4bb70258b5974c08d4aa7d9702a07d
Widyarso Joko Purnomo <widyarso.purnomo@visionet.co.id>
2018-02-01 14:47:38 +0700

    Jp branch

--- app/src/main/java/com/dicoding/made/myflexiblefragment/MainActivity.java
+++ app/src/main/java/com/dicoding/made/myflexiblefragment/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Test new branch
+        //Test Jp Branch
 
         FragmentManager mFragmentManager = getSupportFragmentManager();
         FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
